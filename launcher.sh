56cd3d8b5e4921fd761db862aa5cc62c48eeea3d
Jeff Hoover <jhoover@jhoover.com>
2017-02-14 18:56:34 +0000

    script

---
+++ launcher.sh
@@ -0,0 +1,4 @@
+echo HELLO
+cd /home/pi/dev
+sudo python3 test.py
+
---
+++ test.py
@@ -0,0 +1,47 @@
+import sys, signal
+import RPi.GPIO as GPIO
+
+RED = 7
+YELLOW = 3
+GREEN = 5
+IR_DETECT = 8
+IR_TRIG = 16
+
+def handle_ctrl_c(signal, frame):
+    extinguish_LEDs()
+    sys.exit(130) # 130 is standard exit code for ctrl-c
+
+def extinguish_LEDs():
+    GPIO.output(IR_TRIG,1)
+    GPIO.output(3,0)
+    GPIO.output(5,0)
+    GPIO.output(7,0)
+
+def setup_LEDs():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup(RED, GPIO.OUT)
+    GPIO.setup(YELLOW, GPIO.OUT)
+    GPIO.setup(GREEN, GPIO.OUT)
+    GPIO.setup(IR_DETECT, GPIO.IN)
+    GPIO.setup(IR_TRIG, GPIO.OUT)
+
+setup_LEDs()
+GPIO.output(YELLOW,0)
+GPIO.output(GREEN,1)
+GPIO.output(RED,0)
+GPIO.output(GREEN,1)
+GPIO.output(IR_TRIG,0)
+
+signal.signal(signal.SIGINT, handle_ctrl_c)
+
+while (1):
+    if GPIO.input(IR_DETECT):
+#      print("HIGH")
+      GPIO.output(GREEN,0)
+      GPIO.output(RED,1)
+    else:
+#       print("LOW")
+       GPIO.output(GREEN,1)
+       GPIO.output(RED,0)
+
