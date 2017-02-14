77f343e226dd00aae3e48811dd8e28c2e40bd405
Jeff Hoover <jhoover@jhoover.com>
2017-02-14 20:07:04 +0000

    initial scoreboard - need debounce

--- test.py
+++ test.py
@@ -1,54 +1,56 @@
 import sys, signal
 import RPi.GPIO as GPIO
-#from Adafruit_LED_Backpack import AlphaNum4
+from Adafruit_LED_Backpack import AlphaNum4
 
-#RED = 7
-#YELLOW = 3
-#GREEN = 5
 IR_DETECT = 8
 IR_TRIG = 16
 
 def handle_ctrl_c(signal, frame):
     extinguish_LEDs()
+    clear_score()
     sys.exit(130) # 130 is standard exit code for ctrl-c
+
+def clear_score():
+    display.print_str("    ")
+    display.write_display()
 
 def extinguish_LEDs():
     GPIO.output(IR_TRIG,1)
-#    GPIO.output(YELLOW,0)
-#    GPIO.output(GREEN,0)
-#    GPIO.output(RED,0)
 
 def setup_LEDs():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
-#    GPIO.setup(RED, GPIO.OUT)
-#    GPIO.setup(YELLOW, GPIO.OUT)
-#    GPIO.setup(GREEN, GPIO.OUT)
     GPIO.setup(IR_DETECT, GPIO.IN)
     GPIO.setup(IR_TRIG, GPIO.OUT)
 
-#display = AlphaNum4.AlphaNum4()
-#display.begin()
+display = AlphaNum4.AlphaNum4()
+display.begin()
 setup_LEDs()
-#GPIO.output(YELLOW,0)
-#GPIO.output(GREEN,1)
-#GPIO.output(RED,0)
-GPIO.output(IR_TRIG,1)
+GPIO.output(IR_TRIG,1) # IR_TRIG will light on detection. LATER: need to hardwire an always on and test for beam break
 
-#display.print_str("11 9")
-#display.write_display()
+display.print_str(" 0 0")
+display.write_display()
+
+score_a = 0
+score_b = 0
 
 signal.signal(signal.SIGINT, handle_ctrl_c)
 
+scores = " 0 0"
 while (1):
     if GPIO.input(IR_DETECT):
-      print("HIGH")
+#      print("HIGH")
       GPIO.output(IR_TRIG,1)
-#      GPIO.output(GREEN,0)
-#      GPIO.output(RED,1)
     else:
-       print("LOW")
-       GPIO.output(IR_TRIG,0)
-#       GPIO.output(GREEN,1)
-#       GPIO.output(RED,0)
+#      print("LOW")
+      score_a += 1
+      if score_a > 99:
+         score_a = 0
+      if score_a < 10:
+          scores = " 0 " + str(score_a)
+      else:
+          scores = " 0" + str(score_a)
+      display.print_str(scores)
+      display.write_display()
+      GPIO.output(IR_TRIG,0)
 
