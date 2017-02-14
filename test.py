ed5d4f0e8fdd8a1aa8b5c3ebbb6183a5d34e281e
Jeff Hoover <jhoover@jhoover.com>
2017-02-14 19:36:24 +0000

    taking out traffic light to get acces to I2C pinswq

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import sys, signal
 import RPi.GPIO as GPIO
 #from Adafruit_LED_Backpack import AlphaNum4
 
-RED = 7
-YELLOW = 3
-GREEN = 5
+#RED = 7
+#YELLOW = 3
+#GREEN = 5
 IR_DETECT = 8
 IR_TRIG = 16
 
@@ -14,27 +14,26 @@ def handle_ctrl_c(signal, frame):
 
 def extinguish_LEDs():
     GPIO.output(IR_TRIG,1)
-    GPIO.output(3,0)
-    GPIO.output(5,0)
-    GPIO.output(7,0)
+#    GPIO.output(YELLOW,0)
+#    GPIO.output(GREEN,0)
+#    GPIO.output(RED,0)
 
 def setup_LEDs():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
-    GPIO.setup(RED, GPIO.OUT)
-    GPIO.setup(YELLOW, GPIO.OUT)
-    GPIO.setup(GREEN, GPIO.OUT)
+#    GPIO.setup(RED, GPIO.OUT)
+#    GPIO.setup(YELLOW, GPIO.OUT)
+#    GPIO.setup(GREEN, GPIO.OUT)
     GPIO.setup(IR_DETECT, GPIO.IN)
     GPIO.setup(IR_TRIG, GPIO.OUT)
 
 #display = AlphaNum4.AlphaNum4()
 #display.begin()
 setup_LEDs()
-GPIO.output(YELLOW,0)
-GPIO.output(GREEN,1)
-GPIO.output(RED,0)
-GPIO.output(GREEN,1)
-GPIO.output(IR_TRIG,0)
+#GPIO.output(YELLOW,0)
+#GPIO.output(GREEN,1)
+#GPIO.output(RED,0)
+GPIO.output(IR_TRIG,1)
 
 #display.print_str("11 9")
 #display.write_display()
@@ -43,11 +42,13 @@ signal.signal(signal.SIGINT, handle_ctrl_c)
 
 while (1):
     if GPIO.input(IR_DETECT):
-#      print("HIGH")
-      GPIO.output(GREEN,0)
-      GPIO.output(RED,1)
+      print("HIGH")
+      GPIO.output(IR_TRIG,1)
+#      GPIO.output(GREEN,0)
+#      GPIO.output(RED,1)
     else:
-#       print("LOW")
-       GPIO.output(GREEN,1)
-       GPIO.output(RED,0)
+       print("LOW")
+       GPIO.output(IR_TRIG,0)
+#       GPIO.output(GREEN,1)
+#       GPIO.output(RED,0)
 
